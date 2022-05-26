a649654c31bbc1fbc045dcf40792731f2f91440a
daniel ouattara <daniel.ouattara@gmx.com>
2022-05-26 12:59:20 +0200

    setup security items &  starting deploy to Heroku

--- 04_Jobs_API/my_solution/app.js
+++ 04_Jobs_API/my_solution/app.js
@@ -5,6 +5,12 @@ const app = express();
 
 const auth = require("./middleware/authentication");
 
+// security
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 // routers modules
 const authRouter = require("./routes/auth");
 const jobRouter = require("./routes/jobs");
@@ -13,10 +19,23 @@ const jobRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.use(express.json());
-// extra packages
+// End of import 
+// ------------------------------------------------------------------
 
-// routes
+// security packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 5000,
+    max: 200,
+    message: { code: 429, message: "Too many connection; Try later !" },
+  })
+);
+
+app.use(express.json());
 
 // Routers
 app.use("/api/v1/auth", authRouter);
