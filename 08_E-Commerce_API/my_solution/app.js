8558de05cd2dd5f8539fc95b378a0cd661603740
daniel ouattara <daniel.ouattara@gmx.com>
2022-06-06 19:41:29 +0200

    AuthRoute structure  development

--- 08_E-Commerce_API/my_solution/app.js
+++ 08_E-Commerce_API/my_solution/app.js
@@ -1,6 +1,7 @@
 require("express-async-errors");
 const express = require("express");
 const app = express();
+const authRouter = require("./routes/authRoutes");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const morgan = require("morgan");
@@ -13,6 +14,7 @@ app.use(express.static("./public"));
 
 app.get("/api/v1", (req, res) => res.send("Welcome to E-commerce API"));
 
+app.use("/api/v1/auth", authRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
