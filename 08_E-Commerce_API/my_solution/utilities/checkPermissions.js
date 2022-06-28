48969e503c16d92e1e3958634b5fcf1adcae4673
danielOuattara <daniel.ouattara@gmx.com>
2022-06-28 05:45:23 +0200

    cleaning chekPermission( ) helper function

--- 08_E-Commerce_API/my_solution/utilities/checkPermissions.js
+++ 08_E-Commerce_API/my_solution/utilities/checkPermissions.js
@@ -1,10 +1,6 @@
 const { UnauthenticatedError } = require("./../errors");
 
 const checkPermissions = (requestUser, ressourceUserId) => {
-  //   console.log("requestUser = ", requestUser);
-  //   console.log("ressourceUserId = ", ressourceUserId);
-  //   console.log("");
-
   if (requestUser.role === "admin") return;
   if (requestUser.userId === ressourceUserId.toString()) return;
   throw new UnauthenticatedError("Access denied");
