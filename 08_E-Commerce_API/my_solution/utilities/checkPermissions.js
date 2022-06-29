2ad0d7b88752f6a41445a796fd49f0dd8d92ad51
danielOuattara <daniel.ouattara@gmx.com>
2022-06-29 09:31:08 +0200

    update checkPermissions: adding .toString( ) to ids

--- 08_E-Commerce_API/my_solution/utilities/checkPermissions.js
+++ 08_E-Commerce_API/my_solution/utilities/checkPermissions.js
@@ -2,7 +2,7 @@ const { UnauthenticatedError } = require("./../errors");
 
 const checkPermissions = (requestUser, ressourceUserId) => {
   if (requestUser.role === "admin") return;
-  if (requestUser._id === ressourceUserId.toString()) return;
+  if (requestUser._id.toString() === ressourceUserId.toString()) return;
   throw new UnauthenticatedError("Access denied");
 };
 
