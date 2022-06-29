239731a049d4dc1d0b34190b539580a4a4e71fdb
danielOuattara <daniel.ouattara@gmx.com>
2022-06-29 06:47:10 +0200

    update checkPermissions

--- 08_E-Commerce_API/my_solution/utilities/checkPermissions.js
+++ 08_E-Commerce_API/my_solution/utilities/checkPermissions.js
@@ -2,7 +2,7 @@ const { UnauthenticatedError } = require("./../errors");
 
 const checkPermissions = (requestUser, ressourceUserId) => {
   if (requestUser.role === "admin") return;
-  if (requestUser.userId === ressourceUserId.toString()) return;
+  if (requestUser._id === ressourceUserId.toString()) return;
   throw new UnauthenticatedError("Access denied");
 };
 
