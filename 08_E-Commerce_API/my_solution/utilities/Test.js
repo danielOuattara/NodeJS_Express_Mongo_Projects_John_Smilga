d855ded451de2bc2383a00c715fd367bae763891
danielOuattara <daniel.ouattara@gmx.com>
2022-06-29 16:55:10 +0200

    testing on Mongoose

--- 08_E-Commerce_API/my_solution/utilities/Test.js
+++ 08_E-Commerce_API/my_solution/utilities/Test.js
@@ -1,2 +1,14 @@
-console.log(new Date(Date.now()));
-console.log(Date.now());
+const { log, clear } = require("console");
+// log(new Date(Date.now()));
+// log(Date.now());
+
+// console.clear()
+const mongoose = require("mongoose");
+
+// console.log("mongoose.Mongoose = ", mongoose.Mongoose.toString());
+// console.log("mongoose = ", mongoose);
+
+// log(mongoose instanceof mongoose.Mongoose); // true
+
+const m = new mongoose.Mongoose();
+log("m = ", m);
