1e65db7fca991e5d2e0ee53e700bf7e8319c2c6e
danielOuattara <daniel.ouattara@gmx.com>
2022-06-29 16:55:40 +0200

    agregation pipe from Compass

---
+++ 08_E-Commerce_API/my_solution/models/temporary.js
@@ -0,0 +1,48 @@
+/* 
+Below is code extracted from Compas. 
+It's about how to extract aggregation usibg the GUI 
+Read MongoDB docs for more information
+*/
+
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+const { ObjectId } = require("mongodb");
+
+/*
+ * Requires the MongoDB Node.js Driver
+ * https://mongodb.github.io/node-mongodb-native
+ */
+
+const agg = [
+  {
+    $match: {
+      product: new ObjectId("62bc483e0862b08bd8508a38"),
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      averageRating: {
+        $avg: "$rating",
+      },
+      numberOfReviews: {
+        $sum: 1,
+      },
+    },
+  },
+];
+
+MongoClient.connect(
+  "",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (connectErr, client) {
+    assert.equal(null, connectErr);
+    const coll = client
+      .db("E-Commerce_API_John_Smilga")
+      .collection("reviews");
+    coll.aggregate(agg, (cmdErr, result) => {
+      assert.equal(null, cmdErr);
+    });
+    client.close();
+  }
+);
