886e9b5099a73e358b8071817fb605152f7a32ce
danielOuattara <daniel.ouattara@gmx.com>
2022-06-29 14:20:36 +0200

    adding "numberOfReviews" to Review to Product model

--- 08_E-Commerce_API/my_solution/models/Product.js
+++ 08_E-Commerce_API/my_solution/models/Product.js
@@ -63,6 +63,10 @@ const ProductSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    numberOfreviews: {
+      type: Number,
+      default:0,
+    },
     user: {
       type: mongoose.Types.ObjectId,
       ref: "User",
