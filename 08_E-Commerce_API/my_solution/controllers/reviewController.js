8e936f08d859186c30b2971e6421425908d492d4
danielOuattara <daniel.ouattara@gmx.com>
2022-06-29 06:31:45 +0200

    update reviewController + adding getAllReviews & getSingleReview controller

--- 08_E-Commerce_API/my_solution/controllers/reviewController.js
+++ 08_E-Commerce_API/my_solution/controllers/reviewController.js
@@ -7,22 +7,21 @@ const { checkPermissions } = require("../utilities");
 
 //---------------------------------------------------------------------
 const createReview = async (req, res) => {
-  console.log("req.body =", req.body);
-
   const product = await Product.findById(req.body.productId);
   if (!product) {
     throw new CustomError.NotFoundError("Product unknown");
   }
 
   const oldReview = await Review.findOne({
-    product: req.body.productId,
-    user: req.user._id,
+    productId: req.body.productId,
+    userId: req.user._id,
   });
   if (oldReview) {
     throw new CustomError.BadRequestError(
       `Cannot create a new review on this product. But you can update your old review `
     );
   }
+
   req.body.userId = req.user._id;
   const review = await Review.create(req.body);
 
@@ -30,18 +29,29 @@ const createReview = async (req, res) => {
     .status(StatusCodes.CREATED)
     .json({ message: "review successfully created", review });
 };
+
 //---------------------------------------------------------------------
 const getAllReviews = async (req, res) => {
-  res.send("get all reviews");
+  const reviews = await Review.find({});
+  res.status(StatusCodes.OK).json({ count: reviews.length, reviews });
 };
+
 //---------------------------------------------------------------------
 const getSingleReview = async (req, res) => {
-  res.send("getSingle review");
+  const review = await Review.findById(req.params.reviewId);
+
+  if (!review) {
+    throw new CustomError.BadRequestError(`review unknown`);
+  }
+
+  res.status(StatusCodes.OK).json({ review });
 };
+
 //---------------------------------------------------------------------
 const updateReview = async (req, res) => {
   res.send("update review");
 };
+
 //---------------------------------------------------------------------
 const deleteReview = async (req, res) => {
   res.send("delete review");
