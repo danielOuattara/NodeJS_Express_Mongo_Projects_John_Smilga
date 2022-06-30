b62fd03cd25baed4ebb114ba940eb252670219cd
danielOuattara <daniel.ouattara@gmx.com>
2022-06-30 10:39:25 +0200

    starting 'createOrder' controller ...

--- 08_E-Commerce_API/my_solution/controllers/orderController.js
+++ 08_E-Commerce_API/my_solution/controllers/orderController.js
@@ -3,9 +3,61 @@ const User = require("./../models/User");
 const CustomError = require("./../errors");
 const { StatusCodes } = require("http-status-codes");
 const path = require("path");
+const { checkPermissions } = require("../utilities");
 
 //------------------------------------------------------------------
+const createOrder = async (req, res) => {
+  const { items, tax, shippingFee } = req.body;
 
+  if (!items || items.length < 1) {
+    throw new CustomError.BadRequestError("Your cart items is empty");
+  }
+  if (!tax || !shippingFee) {
+    throw new CustomError.BadRequestError(
+      "Tax and shipping fee are required with exact amount"
+    );
+  }
+
+  let orderItems = [];
+  let subTotal = 0;
+
+  for (const item of items) {
+    let productInDB = await Product.findById(item.product);
+    if (!productInDB) {
+      throw new CustomError.NotFoundError(
+        `${item.name} not found, please chose another product`
+      );
+    }
+    console.log(productInDB);
+
+    if (productInDB.inventory < 1) {
+      throw new CustomError.NotFoundError(
+        `${item.name} is out of stock, please chose another product`
+      );
+    }
+
+    const { name, price, image, _id } = productInDB;
+    const singleOrderItem = {
+      amount: item.amount,
+      name,
+      price,
+      image,
+      product: _id,
+    };
+
+    // add each valid item to orderItems[]
+    orderItems = [...orderItems, singleOrderItem];
+
+    // for each valid item increment
+    subTotal += item.amount * price;
+  }
+
+  console.log("orderItems = ", orderItems, "\n", "subTotal = ", subTotal);
+
+  res.send("create order");
+};
+
+//------------------------------------------------------------------
 const getAllOrders = async (req, res) => {
   res.send("ger all orders");
 };
@@ -18,11 +70,6 @@ const getSingleOrder = async (req, res) => {
 //------------------------------------------------------------------
 const getCurrentUserOrders = async (req, res) => {
   res.send("get current user orders");
-};
-
-//------------------------------------------------------------------
-const createOrder = async (req, res) => {
-  res.send("create order");
 };
 
 //------------------------------------------------------------------
--- 08_E-Commerce_API/my_solution/utilities/Test.js
+++ 08_E-Commerce_API/my_solution/utilities/Test.js
@@ -1,14 +1,19 @@
-const { log, clear } = require("console");
-// log(new Date(Date.now()));
-// log(Date.now());
+// const { log, clear } = require("console");
+// // log(new Date(Date.now()));
+// // log(Date.now());
 
-// console.clear()
-const mongoose = require("mongoose");
+// // console.clear()
+// const mongoose = require("mongoose");
 
-// console.log("mongoose.Mongoose = ", mongoose.Mongoose.toString());
-// console.log("mongoose = ", mongoose);
+// // console.log("mongoose.Mongoose = ", mongoose.Mongoose.toString());
+// // console.log("mongoose = ", mongoose);
 
-// log(mongoose instanceof mongoose.Mongoose); // true
+// // log(mongoose instanceof mongoose.Mongoose); // true
 
-const m = new mongoose.Mongoose();
-log("m = ", m);
+// const m = new mongoose.Mongoose();
+// log("m = ", m);
+
+let items = [];
+
+console.log("items = ", items);
+console.log(!items && items.length === 0);
