1be7e3d9ea0b21cda71985368891c5b3a2a54e10
danielOuattara <daniel.ouattara@gmx.com>
2022-06-30 16:32:14 +0200

    security packages

--- 08_E-Commerce_API/my_solution/app.js
+++ 08_E-Commerce_API/my_solution/app.js
@@ -2,23 +2,43 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
-// routes
+// security import
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const rateLimiter = require("express-rate-limit");
+const sanitizeExpressMongo = require("express-mongo-sanitize");
+
+// routes import
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes ");
 const productRouter = require("./routes/productRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
 const orderRouter = require("./routes/orderRoutes");
 
-// error handler
+// error handler import
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-// functional middleware
+// functional middleware import
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 
-// app.use(morgan("combined"))
+// security
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+    message: { code: 429, message: "Too many connection; Try later !" },
+  })
+);
+app.use(helmet());
+app.use(xss());
+app.use(sanitizeExpressMongo());
+
+// fonctional middlewares
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET)); // <-- signing cookie
