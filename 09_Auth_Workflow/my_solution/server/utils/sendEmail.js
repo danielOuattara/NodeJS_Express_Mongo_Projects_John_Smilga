fdc1029d5617769a66a274504b51bf94a962522b
danielOuattara <daniel.ouattara@gmx.com>
2022-07-01 14:05:42 +0200

    nodemail utils functions

---
+++ 09_Auth_Workflow/my_solution/server/utils/sendEmail.js
@@ -0,0 +1,24 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async () => {
+  let testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "saul.murray78@ethereal.email",
+      pass: "fYW8wkeB2KnnuNsdUn",
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "bar@example.com, baz@example.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+};
+
+module.exports = sendEmail;
