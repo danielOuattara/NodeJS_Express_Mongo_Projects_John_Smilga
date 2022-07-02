43a8a51adbdd67ea8e11e50918ed40cbce62df36
danielOuattara <daniel.ouattara@gmx.com>
2022-07-02 21:08:37 +0200

    info variable modificiation

--- 09_Auth_Workflow/my_solution/server/utils/sendEmail.js
+++ 09_Auth_Workflow/my_solution/server/utils/sendEmail.js
@@ -1,6 +1,7 @@
 const nodemailer = require("nodemailer");
 
-const sendEmail = async () => {
+//---------------------------------------------------------------
+const sendEmail = async (link) => {
   let testAccount = await nodemailer.createTestAccount();
 
   const transporter = nodemailer.createTransport({
@@ -13,12 +14,19 @@ const sendEmail = async () => {
   });
 
   let info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
-    to: "bar@example.com, baz@example.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
+    from: '" Admin Registration" <ricatti@gmx.fr>', // sender address
+    to: "admi1@test.Ca", // list of receivers
+    subject: "Email verification upon Login", // Subject line
+    html: `<b>Hello, click the link <a href=${link}>Confirm Email</a></b>`, // html body
   });
+
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  console.log("Message sent: %s", info.messageId);
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 };
 
+//---------------------------------------------------------------
 module.exports = sendEmail;
