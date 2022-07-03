1053487524ee844ee00b98070d73f98fccd26549
danielOuattara <daniel.ouattara@gmx.com>
2022-07-03 11:59:13 +0200

    setting up sendVerificationEmail

---
+++ 09_Auth_Workflow/my_solution/server/utils/sendVerificationEmail.js
@@ -0,0 +1,21 @@
+const sendEmail = require("./sendEmail");
+
+//------------------------------------------------------------------
+sendVerificationEmail = async ({
+  name,
+  email,
+  verificationToken,
+  origin,
+}) => {
+  const verifyEmailLink = `${origin}/user/verify-email?token=${verificationToken}&email=${email}`;
+  const message = `<p>Please confirm your email: <a href="${verifyEmailLink}">click here</a> </p>`;
+
+  return sendEmail({
+    to: email,
+    subject: "Email confirmation",
+    html: `<h4> Hello ${name}</h4> ${message} `,
+  });
+};
+
+//-------------------------------------------------------------------
+module.exports = sendVerificationEmail;
