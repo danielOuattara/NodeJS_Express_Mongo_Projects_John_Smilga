1cb363aab21a0cbaad0000013a9877a7453f12ef
danielOuattara <daniel.ouattara@gmx.com>
2022-07-04 11:14:32 +0200

    adding sendResetPasswordEmail function

---
+++ 09_Auth_Workflow/my_solution/server/utils/sendResetPasswordEmail.js
@@ -0,0 +1,21 @@
+const sendEmail = require("./sendEmail");
+
+//------------------------------------------------------------------
+const sendResetPasswordEmail = async ({
+  name,
+  email,
+  passwordToken,
+  origin,
+}) => {
+  const resetPasswordLink = `${origin}/user/reset-password?token=${passwordToken}&email=${email}`;
+  const message = `<p>Please confirm your email: <a href="${resetPasswordLink}">click here</a> </p>`;
+
+  return sendEmail({
+    to: email,
+    subject: "Email confirmation",
+    html: `<h4> Hello ${name}</h4> ${message} `,
+  });
+};
+
+//-------------------------------------------------------------------
+module.exports = sendResetPasswordEmail;
