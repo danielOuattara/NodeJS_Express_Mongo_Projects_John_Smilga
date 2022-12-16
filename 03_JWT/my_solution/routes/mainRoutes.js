1896f6042663218a85e4262c9ea63160622fb114
danielOuattara <daniel.ouattara@gmx.com>
2022-12-16 09:05:50 +0100

    tiny updates

--- 03_JWT/my_solution/routes/mainRoutes.js
+++ 03_JWT/my_solution/routes/mainRoutes.js
@@ -4,6 +4,7 @@ const { login, dashboard } = require("../controllers/mainControllers");
 const authMiddleware = require("../middleware/auth");
 
 router.route("/login").post(login);
+
 // router.route("/dashboard").get(dashboard);
 router.route("/dashboard").get(authMiddleware, dashboard);
 
