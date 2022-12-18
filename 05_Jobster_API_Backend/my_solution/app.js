eadd7471a0782d4360634f0e7b193a7e50c49930
danielOuattara <daniel.ouattara@gmx.com>
2022-12-18 17:44:27 +0100

    remove cors

--- 05_Jobster_API_Backend/my_solution/app.js
+++ 05_Jobster_API_Backend/my_solution/app.js
@@ -3,7 +3,6 @@ require("express-async-errors");
 
 // extra security packages
 const helmet = require("helmet");
-const cors = require("cors");
 const xss = require("xss-clean");
 
 const express = require("express");
@@ -20,7 +19,6 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
 app.use(xss());
 
 // routes
