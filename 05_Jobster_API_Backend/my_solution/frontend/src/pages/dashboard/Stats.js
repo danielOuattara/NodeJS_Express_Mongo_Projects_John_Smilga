0959966e0559025c4b27a42ca7af52657d0493bb
danielOuattara <daniel.ouattara@gmx.com>
2022-12-19 16:41:55 +0100

    frontend formatting

--- 05_Jobster_API_Backend/my_solution/frontend/src/pages/dashboard/Stats.js
+++ 05_Jobster_API_Backend/my_solution/frontend/src/pages/dashboard/Stats.js
@@ -1,11 +1,11 @@
-import { useEffect } from 'react';
-import { StatsContainer, Loading, ChartsContainer } from '../../components';
-import { useDispatch, useSelector } from 'react-redux';
-import { showStats } from '../../features/allJobs/allJobsSlice';
+import { useEffect } from "react";
+import { StatsContainer, Loading, ChartsContainer } from "../../components";
+import { useDispatch, useSelector } from "react-redux";
+import { showStats } from "../../features/allJobs/allJobsSlice";
 
 const Stats = () => {
   const { isLoading, monthlyApplications } = useSelector(
-    (store) => store.allJobs
+    (store) => store.allJobs,
   );
 
   const dispatch = useDispatch();
