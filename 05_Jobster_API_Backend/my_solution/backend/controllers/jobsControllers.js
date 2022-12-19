318a896d7b7232aef13810312d2eaace98beedd2
danielOuattara <daniel.ouattara@gmx.com>
2022-12-20 00:29:14 +0100

    update getAllJobs according to queries in request

--- 05_Jobster_API_Backend/my_solution/backend/controllers/jobsControllers.js
+++ 05_Jobster_API_Backend/my_solution/backend/controllers/jobsControllers.js
@@ -4,8 +4,57 @@ const { BadRequestError, NotFoundError } = require("../errors");
 
 //-------------------------
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
-  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
+  const { search, status, jobType, sort } = req.query;
+
+  // default queryObejct
+  const queryObject = {
+    createdBy: req.user.userId,
+  };
+
+  // updated queryObject according to queries
+  if (search) {
+    queryObject.position = { $regex: search, $options: "i" };
+  }
+  if (status && status !== "all") {
+    queryObject.status = status;
+  }
+  if (jobType && jobType !== "all") {
+    queryObject.jobType = jobType;
+  }
+
+  // sort jobs
+  let sortItem = null;
+  if (sort === "latest") {
+    sortItem = "-createdAt";
+  }
+  if (sort === "oldest") {
+    sortItem = "createdAt";
+  }
+  if (sort === "a-z") {
+    sortItem = "position";
+  }
+  if (sort === "z-a") {
+    sortItem = "-position";
+  }
+
+  // setup pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  const jobs = await Job.find(queryObject)
+    .sort(sortItem)
+    .limit(limit)
+    .skip(skip);
+
+  res.status(StatusCodes.OK).json({
+    jobs,
+    totalJobs,
+    numOfPages,
+  });
 };
 
 //-------------------------
