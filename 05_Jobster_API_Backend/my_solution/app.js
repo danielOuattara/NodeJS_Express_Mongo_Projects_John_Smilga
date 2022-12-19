7b54a805af490c7135804acc88d702cd80a685b1
danielOuattara <daniel.ouattara@gmx.com>
2022-12-19 15:19:38 +0100

    binding backend to local frontend

--- 05_Jobster_API_Backend/my_solution/app.js
+++ 05_Jobster_API_Backend/my_solution/app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 require("express-async-errors");
+const path = require("path");
 
 // extra security packages
 const helmet = require("helmet");
@@ -17,6 +18,7 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.use(express.json());
 app.use(helmet());
 app.use(xss());
@@ -24,6 +26,12 @@ app.use(xss());
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+
+app.get("*", (req, res) => {
+  res
+    .status(200)
+    .sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
