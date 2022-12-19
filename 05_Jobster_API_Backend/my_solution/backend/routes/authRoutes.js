b5645a974e8c1a36b96304805c13f30b863fd94e
danielOuattara <daniel.ouattara@gmx.com>
2022-12-20 00:45:12 +0100

    adding rate limiter for some authRoutes endpoints

--- 05_Jobster_API_Backend/my_solution/backend/routes/authRoutes.js
+++ 05_Jobster_API_Backend/my_solution/backend/routes/authRoutes.js
@@ -6,13 +6,19 @@ const {
   updateUser,
 } = require("../controllers/authControllers");
 const authenticatedUser = require("../middleware/authentication");
-
 const checkTestUser = require("./../middleware/checkTestUser");
-
+const rateLimiter = require("express-rate-limit");
 //--------------------------------------------------------
 
-router.post("/register", register);
-router.post("/login", login);
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message: {
+    msg: "Too many requests from this IP, please try again after 15 minutes",
+  },
+});
+router.post("/register", apiLimiter, register);
+router.post("/login", apiLimiter, login);
 router.patch("/updateUser", authenticatedUser, checkTestUser, updateUser);
 
 module.exports = router;
