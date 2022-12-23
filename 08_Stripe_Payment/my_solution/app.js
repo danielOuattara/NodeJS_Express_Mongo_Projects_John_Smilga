4b3c17e465803fffa99162dfe01a00e3a8012461
danielOuattara <daniel.ouattara@gmx.com>
2022-12-23 12:58:05 +0100

    formatting codes

--- 08_Stripe_Payment/my_solution/app.js
+++ 08_Stripe_Payment/my_solution/app.js
@@ -22,13 +22,13 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+ try {
+  app.listen(port, () =>
+   console.log(`Server is listening on port http://localhost:${port}...`),
+  );
+ } catch (error) {
+  console.log(error);
+ }
 };
 
 start();
--- 08_Stripe_Payment/my_solution/controllers/stripeController.js
+++ 08_Stripe_Payment/my_solution/controllers/stripeController.js
@@ -1,20 +1,19 @@
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE);
 
 const stripeController = async (req, res) => {
-  const { purchase, total_amount, shipping_fee } = req.body;
+ const { purchase, total_amount, shipping_fee } = req.body;
 
-  const calculateOrderAmount = () => {
-    return total_amount + shipping_fee;
-  };
+ const calculateOrderAmount = () => {
+  return total_amount + shipping_fee;
+ };
 
-  const paymentIntent = await  stripe.paymentIntents.create({
-      amount: calculateOrderAmount(),
-      currency: "eur",
+ const paymentIntent = await stripe.paymentIntents.create({
+  amount: calculateOrderAmount(),
+  currency: "eur",
+ });
 
-  })
-
-  console.log("paymentIntent = ", paymentIntent)
-  res.json({clientSecret: paymentIntent.client_secret});
+ console.log("paymentIntent = ", paymentIntent);
+ res.json({ clientSecret: paymentIntent.client_secret });
 };
 
 module.exports = stripeController;
