e595a4ce73d5646eb6ec6fa4e8b4a382ecf31d6d
danielOuattara <daniel.ouattara@gmx.com>
2022-12-23 12:50:11 +0100

    update + add logicfor real email sending

--- 07_Send_Email/my_solution/app.js
+++ 07_Send_Email/my_solution/app.js
@@ -1,27 +1,29 @@
 require("dotenv").config();
 require("express-async-errors");
-
 const express = require("express");
 const app = express();
-
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
-const sendEmail = require("./controllers/sendEmail");
+const { sendEmail, sendEmailRealAccount } = require("./controllers/sendEmail");
 
 app.use(express.json());
 
 // routes
 app.get("/", (req, res) => {
- res.send(`<h1>Email Project </h1> <a href="/send">send email</a>`);
+ res.send(`
+ <h1>Email Project </h1> <a href="/send">send email using ethereal</a>
+ <h1>Email Project </h1> <a href="/sendReal">send real email</a>
+ `);
 });
 
 app.get("/send", sendEmail);
+app.get("/sendReal", sendEmailRealAccount);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3500;
 
 const start = async () => {
  try {
