9476b62c2aec0081561915cd5bcfaa6bed2fbec4
danielOuattara <daniel.ouattara@gmx.com>
2023-01-12 22:12:45 +0100

    formatting

--- 09_E-Commerce_API/my_solution/models/Order.js
+++ 09_E-Commerce_API/my_solution/models/Order.js
@@ -23,38 +23,46 @@ const OrderSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
+
     shippingFee: {
       type: Number,
       required: true,
     },
+
     subTotal: {
       type: Number,
       required: true,
     },
+
     total: {
       type: Number,
       required: true,
     },
+
     orderItems: [SingleOrderItemSchema],
+
     status: {
       type: String,
       enum: ["pending", "failed", "paid", "delivered", "canceled"],
       default: "pending",
     },
+
     user: {
       type: mongoose.Schema.ObjectId,
       ref: "User",
       required: true,
     },
+
     clientSecret: {
       type: String,
       required: true,
     },
+
     paymentIntentId: {
       type: String,
     },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 //----------------------------------------------------------------
